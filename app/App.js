a3f6ba03ee5583dbb7750b4a44bae1c464f2e4e1
Dmitriy <kaishien777@gmail.com>
2020-01-11 17:58:21 +0300

    Install Eslint

--- app/App.js
+++ app/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from "./src/components/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Dewfs</Text>
+      <Navbar text={'Hello React Native'}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  }
 });
