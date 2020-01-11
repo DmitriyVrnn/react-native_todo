137fddedf3d18a8981ef70f6d00e44b2ec91dd84
Dmitriy <kaishien777@gmail.com>
2020-01-11 18:57:08 +0300

    Alert for error

--- app/src/components/AddTodo.js
+++ app/src/components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import { View, StyleSheet, TextInput, Button, Alert } from "react-native";
 
 export const AddTodo = ({ addTodo }) => {
   const [value, setValue] = useState('');
@@ -8,7 +8,7 @@ export const AddTodo = ({ addTodo }) => {
       addTodo(value);
       setValue('');
     } else{
-      // error
+      Alert.alert('Название дела не может быть пустым')
     }
   };
 
