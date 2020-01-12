405ad4b03af2ef0a32675c54dda9902561db7a67
Dmitriy <kaishien777@gmail.com>
2020-01-12 17:41:21 +0300

    Component in variable

--- app/App.js
+++ app/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Navbar } from "./src/components/Navbar";
-import {MainScreen} from "./src/screens/MainScreen";
+import { MainScreen } from "./src/screens/MainScreen";
+import { TodoScreen } from "./src/screens/TodoScreen";
 
 export default function App() {
+  const [todoId, setTodoId] = useState(null);
   const [todoList, setTodoList] = useState([]);
   
   const addTodo = (title) => {
@@ -17,14 +19,17 @@ export default function App() {
     setTodoList(prev => prev.filter(item => item.id !== id))
   };
   
+  let content = (<MainScreen todoList={todoList} addTodo={addTodo} removeTodo={removeTodo} />);
+  
+  if(todoId){
+    content = <TodoScreen/>
+  }
+  
   return (
     <View >
       <Navbar text="Todo" />
       <View style={styles.container}>
-        <MainScreen
-          todoList={todoList}
-          addTodo={addTodo}
-          removeTodo={removeTodo}/>
+        {content}
       </View>
     </View>
   );
